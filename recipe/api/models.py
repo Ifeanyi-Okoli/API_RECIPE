9cc6ab4918b7a5590754b032a7010eeb0eb8b0c0
Okoli Ifeanyi <Okoli.I@bullseye.local>
2023-06-20 19:37:30 +0100

    Recipe app

--- recipe/api/models.py
+++ recipe/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Recipe(models.Model):
     title = models.CharField(max_length=100)
-    ingredients = models.CharField(max_length=250)
+    ingredients = models.TextField()
     time_required = models.CharField(max_length=50)
     instructions = models.TextField()
 
--- recipe/api/views.py
+++ recipe/api/views.py
@@ -2,10 +2,38 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Recipe, Ingredient, Tag
 from .serializers import RecipeSerializer, IngredientSerializer, TagSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import viewsets
 
 # Create your views here.
 
-class RecipeListCreateView(generics.ListCreateAPIView):
+class RecipeListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+class RecipeRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     
+class IngredientListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    
+class IngredientRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
